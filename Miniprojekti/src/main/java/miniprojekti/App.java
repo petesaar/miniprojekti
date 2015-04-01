e4f41946752f06fafa5c445cf286641e020db5a3
petesaar <pete.saarela@pp.inet.fi>
2015-04-01 15:54:53 +0300

    gui alkuun_1

--- Miniprojekti/src/main/java/miniprojekti/App.java
+++ Miniprojekti/src/main/java/miniprojekti/App.java
@@ -1,9 +1,20 @@
 package miniprojekti;
 
+import javax.swing.JFrame;
 
-
-public class App {
+public class App extends JFrame{
+    
+    public App() {
+        add(new UserWindow());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1200, 800);
+        setLocationRelativeTo(null);
+        setTitle("Viitteidenkäsittely");
+        setResizable(false);
+        setVisible(true);
+    }
     
     public static void main(String[] args){
+        new App();
     }
 }
