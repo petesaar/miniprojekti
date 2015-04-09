176911e46d4ffbee220bcc3d63d439323465c655
Timo Mäki <timo.j.maki@helsinki.fi>
2015-04-09 18:17:39 +0300

    Viite rajapinnat

---
+++ Miniprojekti/src/main/java/miniprojekti/Viite/KirjaviiteRajapinta.java
@@ -0,0 +1,18 @@
+package miniprojekti.Viite;
+
+/**
+ *
+ * @author Jeesusteippaajat
+ */
+public interface KirjaviiteRajapinta {
+
+    String getRefrence();
+
+    String getAuthor();
+
+    String getYear();
+
+    String getTitle();
+
+    String getPublisher();
+}
---
+++ Miniprojekti/src/main/java/miniprojekti/Viite/ViiteJoukko.java
@@ -0,0 +1,10 @@
+package miniprojekti.Viite;
+
+/**
+ *
+ * @author Jeesusteippaajat
+ */
+public interface ViiteJoukko {
+
+    Iterable<KirjaviiteRajapinta> getKirjaViitteet();
+}
