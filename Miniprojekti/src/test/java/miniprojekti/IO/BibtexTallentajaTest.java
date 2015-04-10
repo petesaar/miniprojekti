16bc8b43c30278e8f8d678fc10a070ba92fd2ccf
Timo Mäki <timo.j.maki@helsinki.fi>
2015-04-10 19:00:52 +0300

    testausta lisää

--- Miniprojekti/src/test/java/miniprojekti/IO/BibtexTallentajaTest.java
+++ Miniprojekti/src/test/java/miniprojekti/IO/BibtexTallentajaTest.java
@@ -24,7 +24,7 @@ public final class BibtexTallentajaTest {
     private BibtexTallentaja tallentaja;
 
     @SuppressWarnings("unchecked")
-    private void before() {
+    private void yksiViite() {
         kirja = mock(KirjaviiteRajapinta.class);
         when(kirja.getAuthor()).thenReturn("Beck, Kent and Andres, Cynthia");
         when(kirja.getPublisher()).thenReturn("Addison-Wesley Professional");
@@ -52,9 +52,46 @@ public final class BibtexTallentajaTest {
         }
     }
 
+    /*
+     @book{Martin09,
+     author = {Martin, Robert},
+     title = {Clean Code: A Handbook of Agile Software Craftsmanship},
+     year = {2008},
+     publisher = {Prentice Hall},
+     }
+     */
+    @SuppressWarnings("unchecked")
+    private void kaksiViitetta() {
+        kirja = mock(KirjaviiteRajapinta.class);
+        when(kirja.getAuthor()).thenReturn("Beck, Kent and Andres, Cynthia", "Martin, Robert");
+        when(kirja.getPublisher()).thenReturn("Addison-Wesley Professional", "Prentice Hall");
+        when(kirja.getRefrence()).thenReturn("BA04", "Martin09");
+        when(kirja.getTitle()).thenReturn("Extreme Programming Explained: Embrace Change (2nd Edition)", "Clean Code: A Handbook of Agile Software Craftsmanship");
+        when(kirja.getYear()).thenReturn(2004, 2008);
+
+        iteraattori = mock(Iterator.class);
+        when(iteraattori.hasNext()).thenReturn(true, true, false);
+        when(iteraattori.next()).thenReturn(kirja, kirja);
+
+        viitteetIteraatio = mock(Iterable.class);
+        when(viitteetIteraatio.iterator()).thenReturn(iteraattori);
+        viitteet = mock(ViiteJoukko.class);
+        when(viitteet.getKirjaViitteet()).thenReturn(viitteetIteraatio);
+
+        stream = new ByteArrayOutputStream();
+        out = mock(IOOut.class);
+        when(out.getOutputStream()).thenReturn(stream);
+        tallentaja = new BibtexTallentaja(viitteet);
+        try {
+            tallentaja.tallennaStream(out);
+        } catch (IOException ex) {
+            fail(ex.toString());
+        }
+    }
+
     @Test
     public void testTallennus() {
-        before();
+        yksiViite();
         String result = new String(stream.toByteArray(), Charset.forName("UTF-8"));
         String expected = "@book{BA04,\n"
                 + "author = {Beck, Kent and Andres, Cynthia},\n"
@@ -66,14 +103,33 @@ public final class BibtexTallentajaTest {
     }
 
     @Test
+    public void kaksiViitettaToimii() {
+        kaksiViitetta();
+        String result = new String(stream.toByteArray(), Charset.forName("UTF-8"));
+        String expected = "@book{BA04,\n"
+                + "author = {Beck, Kent and Andres, Cynthia},\n"
+                + "title = {Extreme Programming Explained: Embrace Change (2nd Edition)},\n"
+                + "year = {2004},\n"
+                + "publisher = {Addison-Wesley Professional},\n"
+                + "}\n"
+                + "@book{Martin09,\n"
+                + "author = {Martin, Robert},\n"
+                + "title = {Clean Code: A Handbook of Agile Software Craftsmanship},\n"
+                + "year = {2008},\n"
+                + "publisher = {Prentice Hall},\n"
+                + "}\n";
+        assertEquals(expected, result);
+    }
+
+    @Test
     public void iOOutKutsutaan() {
-        before();
+        yksiViite();
         verify(out, times(1)).getOutputStream();
     }
 
     @Test
     public void viiteKokoelmaaKutsutaan() {
-        before();
+        yksiViite();
         verify(viitteet, times(1)).getKirjaViitteet();
     }
 
