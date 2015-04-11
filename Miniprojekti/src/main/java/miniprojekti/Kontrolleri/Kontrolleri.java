7236e11abd4a9057a458c47eee3ccd7c9c6cbbf1
salmirinne <ssalmiri@cs.helsinki.fi>
2015-04-11 22:02:41 +0300

    Kontrollerin muokkausta

--- Miniprojekti/src/main/java/miniprojekti/Kontrolleri/Kontrolleri.java
+++ Miniprojekti/src/main/java/miniprojekti/Kontrolleri/Kontrolleri.java
@@ -2,29 +2,50 @@
 package miniprojekti.Kontrolleri;
 
 import java.util.ArrayList;
+import java.util.List;
 import miniprojekti.Viite.Kirjaviite;
 
 /**
  * Luokasta tehdään ohjausolio, joka toimii yhteistyössä muiden luokkien kanssa
  * @author Jeesusteippaajat 
  */
-public class Kontrolleri extends Kirjaviite {
+public class Kontrolleri {
     
-    private ArrayList <Kirjaviite> kirjaviitteet = new ArrayList();
+    private List<Kirjaviite> kirjaviitteet = new ArrayList();
     
-    // 
-    public boolean luoViite (Kontrolleri ohjausOlio) {
-        Kirjaviite uusi = ohjausOlio;
-        if (uusi == null) return false;
-        kirjaviitteet.add(uusi);
-        return true;
+    // luodaan uusi kirjaviite
+    public boolean luoKirjaviite(String reference, String author, String title, 
+            String year, String booktitle, String publisher, String pages, String address, 
+            String volume, String number, String journal) {
+        Kirjaviite viite;
+        try {
+            // jos viitteen luominen epäonnistuu näyttää kontrolleri näkymälle virheen
+            viite = new Kirjaviite(reference, author, title, year, booktitle, 
+                        publisher, pages, address, volume, number, journal);
+        } catch (Exception e) {
+            // TODO: kirjaviite voisi antaa lisätietoa syötteen epäonnistuessa
+            return false;
+        }
+        return kirjaviitteet.add(viite);
     }
     
     // listaa viitteet käyttöliittymää ja tallennusta varten
-    public ArrayList listaaViitteet () {
+    public List<Kirjaviite> listaaViitteet () {
         return kirjaviitteet;
     }
     
-    // lisätäänkö metodi viitteiden levytallennusta varten?
-    // public void tallennaViitteet () { }
+    // palauta viimeksi lisätty kirjaviite
+    public Kirjaviite haeViimeksiLisattyKirjaviite() {
+        return kirjaviitteet.get(kirjaviitteet.size() - 1);
+    }
+    
+    // lisätäänkö metodi viitteiden levytallennusta varten
+    public void tallennaViitteet () {
+        
+    }
+    
+    // metodi luokkien lataamiseen levyltä
+    public void haeViitteet () {
+        
+    }
 }
