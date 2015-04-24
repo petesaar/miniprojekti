77178ea4531d7c986cc3ba9413466743ae3133ba
Iiro Aalto <iiroaalto@gmail.com>
2015-04-24 20:58:36 +0300

    JSON-tallennus toimii

--- Miniprojekti/src/main/java/miniprojekti/JSON/JsonIO.java
+++ Miniprojekti/src/main/java/miniprojekti/JSON/JsonIO.java
@@ -1,11 +1,18 @@
 package miniprojekti.JSON;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Map;
+import miniprojekti.Viite.Artikkeliviite;
+import miniprojekti.Viite.Inproceedings;
+import miniprojekti.Viite.Kirjaviite;
 import miniprojekti.Viite.Viite;
 import org.json.JSONWriter;
 import miniprojekti.Viite.ViiteJoukko;
 import miniprojekti.Viite.ViitejoukkoImpl;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 /**
  *
@@ -45,12 +52,35 @@ public class JsonIO {
         }
     }
     
-    public ViiteJoukko lataa(){
+    public ViiteJoukko lataa() throws Exception{
+        ViitejoukkoImpl viitteet = new ViitejoukkoImpl();
+        try{
+            JSONTokener tokener = new JSONTokener(new FileReader(filename));
+            JSONArray array = new JSONArray(tokener);
+            for(int i = 0; i < array.length(); i++){
+                JSONObject object = array.getJSONObject(i);
+                String type = object.getString("type");
+                if(type.equals("book")){
+                    viitteet.save(new Kirjaviite(object.getString("bibtexkey"),object.getString("author"),object.getString("title"),object.getString("year"),object.getString("publisher"),
+                                                 object.getString("booktitle"), object.getString("pages"), object.getString("address"),object.getString("volume"),object.getString("number"),
+                                                 object.getString("journal")));
+                } else if(type.equals("inproceedings")){
+                    viitteet.save(new Inproceedings(object.getString("bibtexkey"),object.getString("author"),object.getString("title"),object.getString("year"),object.getString("booktitle"),
+                                                    object.getString("editor"),object.getString("volnum"),object.getString("series"),object.getString("pages"),object.getString("address"),
+                                                    object.getString("month"),object.getString("organisation"),object.getString("publisher"),object.getString("key"),object.getString("note")));
+                } else if(type.equals("article")){
+                    viitteet.save(new Artikkeliviite(object.getString("bibtexkey"),object.getString("author"),object.getString("title"),object.getString("journal"),object.getString("year"),
+                                                    object.getString("volume"),object.getString("number"),object.getString("pages"),object.getString("month"),object.getString("note"),
+                                                    object.getString("key")));
+                }
+                
+            }    
+            
+            
+        } catch (Exception e){
+            throw e;
+        }
         
-        return new ViitejoukkoImpl();
-    }
-
-    
-    
-    
+        return viitteet;
+    }  
 }
