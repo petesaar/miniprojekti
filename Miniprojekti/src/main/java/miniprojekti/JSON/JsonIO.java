609abb43ecdc99dd9c3204a546bded6d1ddb25d4
Iiro Aalto <iiroaalto@gmail.com>
2015-04-26 04:54:00 +0300

    refaktorointia ja testejä

--- Miniprojekti/src/main/java/miniprojekti/JSON/JsonIO.java
+++ Miniprojekti/src/main/java/miniprojekti/JSON/JsonIO.java
@@ -11,13 +11,16 @@ import org.json.JSONWriter;
 import miniprojekti.Viite.ViiteJoukko;
 import miniprojekti.Viite.ViitejoukkoImpl;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+
 /**
- *
+ * Tallentaa viitteitä JSON-tiedostoon. 
  * @author Jeesusteippaajat
  */
+
 public class JsonIO {
     
     private String filename;
@@ -25,23 +28,19 @@ public class JsonIO {
     public JsonIO(String filename){
         this.filename = filename;
     }
-    
+    /**
+     * Tallentaa kaikki viitejoukon viitteet tiedostoon.
+     * @param viitteet
+     * @return false, jos tallennus epäonnistui.
+     */
     public boolean tallenna(ViiteJoukko viitteet){         
-        try{
-            
+        try{   
             FileWriter filew = new FileWriter(filename);
             JSONWriter writer = new JSONWriter(filew);        
             
             writer.array();
             for(Viite viite: viitteet.getViitteet()){
-                writer.object().key("type").value(viite.getType());
-                writer.key("bibtexkey").value(viite.getBibtexkey());
-                Map<String, String> fields = viite.getFields();
-                
-                for(String key : fields.keySet()){
-                    writer.key(key).value(fields.get(key));
-                }
-                writer.endObject();
+                tallennaViite(writer, viite);
             
             }
             writer.endArray();
@@ -51,36 +50,67 @@ public class JsonIO {
             return false;
         }
     }
+
     
-    public ViiteJoukko lataa() throws Exception{
+    /**
+     * Lataa viitteet tiedostosta. 
+     * @return Viitejoukko, jossa ladatut viitteet. Null jos lataaminen epäonnistui.
+     */
+    public ViiteJoukko lataa(){
         ViitejoukkoImpl viitteet = new ViitejoukkoImpl();
         try{
-            JSONTokener tokener = new JSONTokener(new FileReader(filename));
+            FileReader reader = new FileReader(filename);
+            JSONTokener tokener = new JSONTokener(reader);
             JSONArray array = new JSONArray(tokener);
             for(int i = 0; i < array.length(); i++){
                 JSONObject object = array.getJSONObject(i);
                 String type = object.getString("type");
                 if(type.equals("book")){
-                    viitteet.save(new Kirjaviite(object.getString("bibtexkey"),object.getString("author"),object.getString("title"),object.getString("year"),object.getString("publisher"),
-                                                 object.getString("booktitle"), object.getString("pages"), object.getString("address"),object.getString("volume"),object.getString("number"),
-                                                 object.getString("journal")));
+                    luoKirjaviite(viitteet, object);
                 } else if(type.equals("inproceedings")){
-                    viitteet.save(new Inproceedings(object.getString("bibtexkey"),object.getString("author"),object.getString("title"),object.getString("year"),object.getString("booktitle"),
-                                                    object.getString("editor"),object.getString("volnum"),object.getString("series"),object.getString("pages"),object.getString("address"),
-                                                    object.getString("month"),object.getString("organisation"),object.getString("publisher"),object.getString("key"),object.getString("note")));
+                    luoInproceedings(viitteet, object);
                 } else if(type.equals("article")){
-                    viitteet.save(new Artikkeliviite(object.getString("bibtexkey"),object.getString("author"),object.getString("title"),object.getString("journal"),object.getString("year"),
-                                                    object.getString("volume"),object.getString("number"),object.getString("pages"),object.getString("month"),object.getString("note"),
-                                                    object.getString("key")));
-                }
-                
+                    luoArtikkeliviite(viitteet, object);
+                }    
             }    
-            
+            reader.close();
             
         } catch (Exception e){
-            throw e;
+            return null;
         }
         
         return viitteet;
     }  
+    
+    //Kirjoittaa yhden viitteen jsoniin.
+    private void tallennaViite(JSONWriter writer, Viite viite) throws JSONException {
+        writer.object().key("type").value(viite.getType());
+        writer.key("bibtexkey").value(viite.getBibtexkey());
+        Map<String, String> fields = viite.getFields();
+        
+        for(String key : fields.keySet()){
+            writer.key(key).value(fields.get(key));
+        }
+        writer.endObject();
+    }
+
+    //Nämä luovat viiteolion JSONObject-oliosta.
+    
+    private void luoKirjaviite(ViitejoukkoImpl viitteet, JSONObject object) throws JSONException {
+        viitteet.save(new Kirjaviite(object.getString("bibtexkey"),object.getString("author"),object.getString("title"),object.getString("year"),object.getString("publisher"),
+                object.getString("booktitle"), object.getString("pages"), object.getString("address"),object.getString("volume"),object.getString("number"),
+                object.getString("journal")));
+    }
+
+    private void luoInproceedings(ViitejoukkoImpl viitteet, JSONObject object) throws JSONException {
+        viitteet.save(new Inproceedings(object.getString("bibtexkey"),object.getString("author"),object.getString("title"),object.getString("year"),object.getString("booktitle"),
+                object.getString("editor"),object.getString("volnum"),object.getString("series"),object.getString("pages"),object.getString("address"),
+                object.getString("month"),object.getString("organisation"),object.getString("publisher"),object.getString("note"),object.getString("key")));
+    }
+
+    private void luoArtikkeliviite(ViitejoukkoImpl viitteet, JSONObject object) throws JSONException {
+        viitteet.save(new Artikkeliviite(object.getString("bibtexkey"),object.getString("author"),object.getString("title"),object.getString("journal"),object.getString("year"),
+                object.getString("volume"),object.getString("number"),object.getString("pages"),object.getString("month"),object.getString("note"),
+                object.getString("key")));
+    }
 }
--- Miniprojekti/src/test/java/miniprojekti/JSON/JsonIOTest.java
+++ Miniprojekti/src/test/java/miniprojekti/JSON/JsonIOTest.java
@@ -1,8 +1,15 @@
 package miniprojekti.JSON;
 
+import java.io.File;
+import miniprojekti.Viite.Artikkeliviite;
+import miniprojekti.Viite.Inproceedings;
+import miniprojekti.Viite.Kirjaviite;
+import miniprojekti.Viite.Viite;
+import miniprojekti.Viite.ViiteJoukko;
+import miniprojekti.Viite.ViitejoukkoImpl;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class JsonIOTest {
     
@@ -15,7 +22,51 @@ public class JsonIOTest {
     }
 
     @Test
-    public void testSomeMethod() {
+    public void tallennaLuoTiedostonJosSitäEiOle() {
+        
+        JsonIO io = new JsonIO("test.json");
+        io.tallenna(new ViitejoukkoImpl());
+        File file = new File("test.json");
+        if(file.exists()){
+            file.delete();
+        } else fail();       
     }
     
+    @Test
+    public void latausToimiiKirjaviittellä(){
+        tallennaJaLataaViite(new Kirjaviite("key","auth","title","2013","pub","as","ds","asd","dd","aet","sdfd"));       
+    }
+    
+    @Test
+    public void latausToimiiArtikkeliviitteellä(){
+        tallennaJaLataaViite(new Artikkeliviite("ref","auth","title","jour","1234","vol","aet","tea","etq","asd","reee"));
+    }
+    
+    @Test
+    public void latausToimiiInproceedingsViitteellä(){
+        tallennaJaLataaViite(new Inproceedings("key","auth","title","1234","booktitle","aef","as","egage","req","asd","gea","geg","aegasd","eaae","gegegeg"));
+    }
+    
+    private void tallennaJaLataaViite(Viite viite){
+        ViitejoukkoImpl joukko = new ViitejoukkoImpl();
+        joukko.save(viite);
+        
+        JsonIO io = new JsonIO("test.json");
+        io.tallenna(joukko);
+        
+        ViiteJoukko ladatut = io.lataa();
+        Viite v  = ladatut.getViitteet().get(0);
+        
+        if(v.getClass().equals(viite.getClass())){
+            if(v.getBibtexkey().equals(viite.getBibtexkey())){
+                for(String key : viite.getFields().keySet()){
+                    String a = viite.getFields().get(key);
+                    String b = v.getFields().get(key);
+                    if(!a.equals(b)) fail("Kentän "+key+" tulee olla "+a+" mutta olikin "+b);
+                }
+            } else fail();
+        } else fail();
+        File file = new File("test.json");
+        System.out.println(file.delete());
+    }
 }
